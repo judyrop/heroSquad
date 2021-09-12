5a33fbb81f544004d205ba011109d5fd9377c46e
Your Name <youremail@domain.com>
2021-09-12 22:26:20 +0300

    created template and public directory and added templates file and wrote codes inside and added an image

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,15 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
 import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
-      get("/hero",(request,response) -> "Hero squad!");
+//      get("/hero",(request,response) -> "Hero squad!");
+        staticFileLocation("/public");
+        get("/hero",(request,response)->{
+            return new ModelAndView(new HashMap(), "hero.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
