b56803ba722954839a8841f2409379c7161855d5
Your Name <youremail@domain.com>
2021-09-13 09:46:11 +0300

    created test methods and all passed

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -2,12 +2,28 @@ package models;
 
 public class Hero {
     private String name;
-    public Hero(String name) {
+    private int age;
+    private String power;
+    private String weakness;
+    public  Hero(String name, int age, String power, String weakness) {
         this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
     }
     public String getName() {
       return name;
     }
-  }
+    public int getAge() {
+        return age;
+    }
+
+    public String getPower() {
+        return power;
+    }
+    public String getWeakness() {
+        return weakness;
+    }
+}
 
 
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
 
+
     @BeforeEach
     void setUp() {
     }
@@ -16,17 +17,32 @@ class HeroTest {
     @AfterEach
     void tearDown() {
     }
-
     @Test
     public void newHero_instantiatesCorrectly_true() {
-        Hero newHero = new Hero("1:hero");
+        Hero newHero = new Hero("Alvin",18,"flying", "fire");
         assertTrue(newHero instanceof Hero);
     }
     @Test
     public void newHero_getName_String() {
-        Hero newHero = new Hero("hero");
-        assertEquals("hero", newHero.getName());
+        Hero newHero = new Hero("Alvin",18,"flying", "fire");
+        assertEquals("Alvin", newHero.getName());
     }
 
+    @Test
+    public void newHero_getAge_Int() {
+        Hero newHero =  new Hero("Alvin",18,"flying", "fire");
+        assertEquals(18, newHero.getAge());
+    }
 
+    @Test
+    public void newHero_getPower_String() {
+        Hero newHero = new Hero("Alvin",18,"flying", "fire");
+        assertEquals("flying", newHero.getPower());
+    }
+
+    @Test
+    public void newHero_getWeakness_String() {
+        Hero newHero = new Hero("Alvin",18,"flying", "fire");
+        assertEquals("fire", newHero.getWeakness());
+    }
 }
