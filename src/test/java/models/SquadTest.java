39b39e14afcbb6282a103d6c62b8192e9cb0525b
Your Name <youremail@domain.com>
2021-09-13 14:55:11 +0300

    created tests for squad

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,25 @@ class SquadTest {
 
     @AfterEach
     void tearDown() {
+    }
+    @Test
+    public void newSquad_instantiatesCorrectly_true()throws Exception {
+        Squad newSquad = new Squad("Lions",5,"Iron-Group");
+        assertTrue(newSquad instanceof Squad);
+    }
+    @Test
+    public void newSquad_getName_String() throws Exception{
+        Squad newSquad = new Squad("Lions",5,"Iron-Group");
+        assertEquals("Lions", newSquad.getName());
+    }
+    @Test
+    public void newSquad_getSize_Int() throws Exception{
+        Squad newSquad = new Squad("Lions",5,"Iron-Group");
+        assertEquals(5, newSquad.getSize());
+    }
+    @Test
+    public void newSquad_getCause_String() throws Exception{
+        Squad newSquad = new Squad("Lions",5,"Iron-Group");
+        assertEquals("Iron-Group", newSquad.getCause());
     }
 }
