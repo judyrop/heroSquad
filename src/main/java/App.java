3c064c9db33000b67ede3788035c68d54a4d5451
Your Name <youremail@domain.com>
2021-09-13 11:21:15 +0300

    added a success part after adding a hero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,39 +10,45 @@ public class App {
     public static void main(String[] args) {
 //      get("/hero",(request,response) -> "Hero squad!");
         staticFileLocation("/public");
-        get("/",(request,response)->{
+        get("/",(req,res)->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(new HashMap(), "index.hbs");
+        }, new HandlebarsTemplateEngine());
+       get("/hero",(request,response)->{
            Map<String, Object> model = new HashMap<String, Object>();
-          return new ModelAndView(new HashMap(), "index.hbs");
-         }, new HandlebarsTemplateEngine());
-//        get("/hero",(request,response)->{
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            return new ModelAndView(new HashMap(), "hero.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        get("/hero-form", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            return new ModelAndView(model, "hero-form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        get("/squad-form", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            return new ModelAndView(model, "squad-form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        post("/squad-form", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//
-//            String InputtedSquadName = request.queryParams("SquadName");
-//            request.session().attribute("SquadName", InputtedSquadName);
-//            model.put("SquadName", InputtedSquadName);
-//
-//            return new ModelAndView(model, "squad-form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//        post("/hero-form", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//
-//            String InputtedName = request.queryParams("Name");
-//            request.session().attribute("Name", InputtedName);
-//            model.put("Name", InputtedName);
-//
-//            return new ModelAndView(model, "hero-form.hbs");
-//        }, new HandlebarsTemplateEngine());
+           return new ModelAndView(new HashMap(), "hero.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/hero-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/squad-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/squad-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            String InputtedSquadName = request.queryParams("SquadName");
+            request.session().attribute("SquadName", InputtedSquadName);
+            model.put("SquadName", InputtedSquadName);
+
+            return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/hero-form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            String InputtedName = request.queryParams("Name");
+            request.session().attribute("Name", InputtedName);
+            model.put("Name", InputtedName);
+
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/hero/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("name");
+            Hero newHero = new Hero("name",18,"flying","fire");
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
